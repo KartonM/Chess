d9f2c6c3f977c89b32b5c106825bf680e1d15d26
blondyn526j <blondyn526j@gmail.com>
2016-12-21 21:46:58 +0100

    Pierwsza wersja - bez błędu

--- Szachy/Szachy/Properties/Resources.Designer.cs
+++ Szachy/Szachy/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Szachy.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap WhiteKnight {
-            get {
-                object obj = ResourceManager.GetObject("WhiteKnight", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
